b2c528ee19a8ed06b974dfb7c551e6296833e56e
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-08 18:42:34 +0530

    codemirror attached

---
+++ src/components/Editor.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+import CodeMirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css"
+import "codemirror/theme/dracula.css";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+
+const Editor = () => {
+    useEffect(() => {
+        async function init() {
+            CodeMirror.fromTextArea(document.getElementById("realTimeEditor"), {
+                mode: { name: "javascript", json: true },
+                mode: "css",
+                theme: "dracula",
+                autoCloseTags: true,
+                autoCloseBrackets: true,
+                lineNumbers: true,
+                lineWrapping: true,
+                scrollbarStyle: "null",
+            });
+        }
+        init();
+    }, []);
+
+    return <textarea id="realTimeEditor" />;
+};
+
+export default Editor;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return (
-    <div>EditorPage</div>
-  )
-}
+    const [client, setClient] = useState([
+        { socketId: "1", userName: "John Doe" },
+        { socketId: "2", userName: "Aman" },
+        { socketId: "3", userName: "Suman" },
+    ]);
 
-export default EditorPage
+    return (
+        <div className="editorWrapper">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img src="/code-sync.png" alt="logo" />
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientList">
+                        {client.map((client) => (
+                            <Client
+                                key={client.socketId}
+                                username={client.userName}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <button className="btn copyBtn">Copy ROOM ID</button>
+                <button className="btn leaveBtn">Leave</button>
+            </div>
+            <div className="editor">
+                <Editor />
+            </div>
+        </div>
+    );
+};
+
+export default EditorPage;
